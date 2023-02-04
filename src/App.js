b050c1acc5a578b28c8df3ae7da371d49f033215
sujauddin <sujauddinkazi282538@gmail.com>
2023-02-04 23:42:32 +0530

    react 4 assingment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>THIS IS THE IMAGE </h1>
+      <img src="https://cdn.mos.cms.futurecdn.net/LV4XEGHyUitdZRcYXjXg5S-320-80.jpg" alt=""></img>
+  
     </div>
   );
 }
